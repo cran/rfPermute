ecca1dce3a83dea4c37915760c659dab27c18aa0
Eric Archer <eric.archer@noaa.gov>
2012-09-20 00:00:00 +0000

    version 1.4

--- R/rfPermute.default.R
+++ R/rfPermute.default.R
@@ -1,10 +1,9 @@
 #' @rdname rfPermute
-#' @usage \method{rfPermute}{default}(x, y, \dots, nrep = 100, clust.opts = NULL)
+#' @usage \method{rfPermute}{default}(x, y, \dots, nrep = 1000)
 
-rfPermute.default <- function(x, y, ..., nrep = 100, clust.opts = NULL) {  
+rfPermute.default <- function(x, y, ..., nrep = 1000) {  
   # Takes same arguments as 'randomForest.default', plus
   #   'nrep': number of permutation replicates
-  #   'clust.opts' : list of options to set up clusters for package 'parallel'
   #
   # Returns 'randomForest' object with:
   #   'null.dist' : 3 element named list with null distribution matrices
@@ -13,7 +12,7 @@ rfPermute.default <- function(x, y, ..., nrep = 100, clust.opts = NULL) {
   #      and 'pval' a matrix of p-values for each predictor (rows) on each
   #      importance metric (columns).
   #
-  #  7/29/2011
+  #  9/20/2012
 
   stopifnot(require(randomForest, quietly = TRUE))
   orig.call <- match.call()
@@ -34,53 +33,27 @@ rfPermute.default <- function(x, y, ..., nrep = 100, clust.opts = NULL) {
   imp.names <- colnames(rf$importance)
   imp.names <- imp.names[c(length(imp.names) - 1, length(imp.names))]
   
-  # permutes 'y' in rf.call 'nrep' times and runs randomForest
-  permute.func <- function(nrep, rf.call) {
-    lapply(1:nrep, function(i) {
+  # permutes 'y' in rf.call 'nrep' times and runs randomForest  
+  if(nrep > 0) {
+    lapply.func <- if(require(multicore, quietly = T)) mclapply else lapply
+    importance.perm <- lapply.func(1:nrep, function(i) {
       rf.call$y <- sample(rf.call$y)
       eval(rf.call)$importance
     })
-  }
-  
-  # setup clusters if 'clust.opt' provided
-  importance.perm <- if(!is.null(clust.opts)) {
-    require(parallel, quietly = TRUE)
     
-    clust <- if(is.vector(clust.opts) & length(clust.opts) == 1 & is.integer(clust.opts)) {
-      makeCluster(clust.opts)
-    } else if(is.list(clust.opts)) {
-      do.call(makeCluster, clust.opts)
-    } else {
-      makeCluster(detectCores())
-    }
+    # create null distribution for each variable  
+    rf$null.dist <- sapply(imp.names, function(imp.type) {
+      t(sapply(1:length(importance.perm), function(i) importance.perm[[i]][, imp.type]))
+    }, simplify = FALSE)
     
-    if(length(clust) == 1) { # if only one cluster was specified, do normal permutation
-      stopCluster(clust)
-      permute.func(nrep, rf.call)
-    } else {
-      clusterEvalQ(clust, library(randomForest))
-      clusterSetRNGStream(clust)
-      rep.split <- clusterSplit(clust, 1:nrep)
-      result <- clusterApply(clust, sapply(rep.split, length), permute.func, rf.call = rf.call)
-      stopCluster(clust) 
-      do.call(c, result)    
-    }
-  } else {
-    permute.func(nrep, rf.call)
-  }
-    
-  # create null distribution for each variable  
-  rf$null.dist <- sapply(imp.names, function(imp.type) {
-    t(sapply(1:length(importance.perm), function(i) importance.perm[[i]][, imp.type]))
-  }, simplify = FALSE)
-  
-  # calculate p-value of observed importance metrics
-  rf$null.dist$pval <- t(sapply(rownames(rf$importance), function(pred) {
-    result <- sapply(names(rf$null.dist), function(imp.type) {
-      num.perm.gte <- sum(rf$null.dist[[imp.type]][, pred] >= rf$importance[pred, imp.type]) + 1
-      num.perm.gte / (nrow(rf$null.dist[[imp.type]]) + 1)
-    }, USE.NAMES = TRUE)
-  }, USE.NAMES = TRUE))
+    # calculate p-value of observed importance metrics
+    rf$null.dist$pval <- t(sapply(rownames(rf$importance), function(pred) {
+      result <- sapply(names(rf$null.dist), function(imp.type) {
+        num.perm.gte <- sum(rf$null.dist[[imp.type]][, pred] >= rf$importance[pred, imp.type]) + 1
+        num.perm.gte / (nrow(rf$null.dist[[imp.type]]) + 1)
+      }, USE.NAMES = TRUE)
+    }, USE.NAMES = TRUE))
+  } 
   
   rf$call <- orig.call
   class(rf) <- c("rfPermute", "randomForest")
